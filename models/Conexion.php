54def4b78bc732b7b6fddfdaf0ad0ad72308ad17
FelipeOctavio94 <69543482+FelipeOctavio94@users.noreply.github.com>
2021-06-26 19:57:50 -0400

    coneccion

--- models/Conexion.php
+++ models/Conexion.php
@@ -6,9 +6,13 @@ use PDO;
 use PDOException;
 
 class Conexion{
-    public static $user="root";
-    public static $pass="";
-    public static $URL="mysql:host=localhost;dbname=mercado";
+    //public static $user="root";
+    //public static $pass="";
+    //public static $URL="mysql:host=localhost;dbname=mercado";
+
+      public static $user="ukfqa48l4dcg86rr";
+      public static $pass="TfXviTyj0uvS0a4OWamP";
+      public static $URL="mysql:host=bymr6vwwkdai42mtnopv-mysql.services.clever-cloud.com;dbname=bymr6vwwkdai42mtnopv";
 
     public static function conector(){
         try{
