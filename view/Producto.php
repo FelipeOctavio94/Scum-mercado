2486631d8863df9dcb3c663231db826982fb48ba
FelipeOctavio94 <69543482+FelipeOctavio94@users.noreply.github.com>
2021-06-26 20:56:19 -0400

    arreglo js

--- view/Producto.php
+++ view/Producto.php
@@ -122,6 +122,6 @@
     <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>  
  
-    <script src="../js/Productos.js"></script>
+    <script src="../js/productos.js"></script>
 </body>
 </html>
